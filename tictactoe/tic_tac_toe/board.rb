ba18e7dfc4113ba03f0e19ab9d01b024a74a8ceb
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-02 19:10:24 +0300

    create the winner function that checks for the winner

--- tictactoe/tic_tac_toe/board.rb
+++ tictactoe/tic_tac_toe/board.rb
@@ -17,4 +17,22 @@ class Board
     def full?
         @field.all? { |cell| cell=="X" || cell == "O"}
     end
+
+    def winner
+        winning_condition = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]
+
+        ]
+
+        winning_condition.each do |condition| 
+            return "X"
+            if condition.all? { |cell| @field[cell] == "X"}
+            return "O" 
+            if condition.all? { |cell| @field[cell] == "O"}
+            end
+            return false
+        end
+    end
 end
