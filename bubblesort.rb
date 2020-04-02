3bd9a9b14cfa0967cb9d68384fe5c2d636521f7b
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-02 18:32:52 +0300

    add edge cases for the function

--- bubblesort.rb
+++ bubblesort.rb
@@ -3,23 +3,34 @@ def bubble_sort(array)
     # get the array length coz we will use it to count how many times we loop
     n = array.length
     #Start the loop
-    loop {
-        # set our swapped check to be false
-        swapped = false
-        #Iterate through the array n-1 times
-        (n-1).times { |i| 
-            #Check if left side is greater than the right side
-            if array[i] > array[i+1]
-                #Swap the values and change the status of swap
-                array[i], array[i+1] = array[i+1], array[i]
-                swapped = true
-            end
-
+    if n == 1 
+         array
+    
+    elsif  n == 0
+        puts "Array is empty"
+    else    
+        loop {
+            # set our swapped check to be false
+            swapped = false
+            #Iterate through the array n-1 times
+            (n-1).times { |i| 
+                #Check if left side is greater than the right side
+                if array[i] > array[i+1]
+                    #Swap the values and change the status of swap
+                    array[i], array[i+1] = array[i+1], array[i]
+                    swapped = true
+                end
+    
+            }
+            break if not swapped
+    
         }
-        break if not swapped 
-
-    }
-    array
-end
-a = bubble_sort([1,3,4,1,5,0])
+        array
+    end
+    
+    end
+    
+a = bubble_sort([1])
 print a 
+
+
