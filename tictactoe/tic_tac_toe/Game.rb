c46e97219219f79e0b1591eb1a526ac0825cdfb9
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-02 20:27:05 +0300

    implement the finish game method and play method

--- tictactoe/tic_tac_toe/Game.rb
+++ tictactoe/tic_tac_toe/Game.rb
@@ -27,4 +27,36 @@ class Game
 
 
     end
+
+    def finish_game
+        case @board.winner
+        when "X"
+            puts "#{@player_1.name} wins!"
+        when "O"
+            puts "#{@player_2.name} wins!"
+        else
+            puts "Its a tie"
+        end
+        puts "Want to play again?"
+        answer = gets.chomp.downcase
+        valid_yes = ["yes","Y"]
+        valid_no = ["no","N"]
+        
+        until valid_yes.any? { |item| answer == item} || valid_no.any? {|item| answer == item}
+            puts "Give either yes or no "
+            answer = gets.chomp.downcase
+        end
+        Game.new.play if valid_yes.any? { |item| answer == item}
+
+    end
+
+    def play
+        self.start
+        until @board.full? || @board.winner
+            self.turn(@player_1)
+            break if @board.full? || @board.winner
+            self.turn(@player_2)
+        end
+        self.finish_game
+    end
 end
