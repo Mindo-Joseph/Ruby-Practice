26070f371242dde06f376e08f938431e27375386
Joseph Mindo <mindo.joseph.mj@gmail.com>
2020-04-02 18:56:55 +0300

    create the show board method

--- tictactoe/tic_tac_toe/board.rb
+++ tictactoe/tic_tac_toe/board.rb
@@ -1,8 +1,16 @@
 class Board
     attr_accessor :field
+
     def initialize
         @field = []
     end
 
-    
+    def show
+        puts "#{@field[0]} | #{@field[1]} | #{@field[2]}"
+        puts "---+---+----"
+        puts "#{@field[3]} | #{@field[4]} | #{@field[5]}"
+        puts "---+---+----"
+        puts "#{@field[6]} | #{@field[7]} | #{@field[8]}"
+        puts "---+---+----"
+    end
 end
